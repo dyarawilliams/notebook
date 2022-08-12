1aae60054af3f3d69239ba9bbc5de8c9dd126fcf
D'yara Williams <dyara.williams@gmail.com>
2022-08-12 16:26:25 -0400

    add current mongostore

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require("express-handlebars")
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 const mainRouter = require('./routes/index')
@@ -37,6 +38,9 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false, 
     saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    })
 }))
 
 // Passport middleware
