61203197b76b691b79edfd93f4625929a84f28e3
D'yara Williams <dyara.williams@gmail.com>
2022-08-12 15:05:25 -0400

    update auth.js route

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,9 @@ router.get('/google/callback',
 
 // @desc Logout user
 // @route /auth/logout
-router.get('/logout', (req, res) => {
+router.get('/logout', (req, res, next) => {
     req.logout(function(err) {
-        if (err) { return next(err); } 
+        if (err) { return next(err) } 
         res.redirect('/');
     });
 })
